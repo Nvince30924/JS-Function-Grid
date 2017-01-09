116cc76b6a3aa85e2e3f258b88d3e419f2ff7a69
Vince Ngitit <vinceallenn7013@queensvoc.org>
2017-01-09 22:19:06 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+setColor (0, 0, "blue");
+setColor (9, 0, "yellow");
+setColor (0, 9, "blue");
+setColor (9, 9, "yellow");
+
+setColor (2, 2, "black");
+setColor (2, 7, "black");
+setColor (5, 4, "black");
+
+setColor (5, 3, "purple");
+setColor (4, 7, "gold");
+setColor (4, 2, "purple");
+setColor (5, 5, "gold");
+setColor (5, 6, "purple");
